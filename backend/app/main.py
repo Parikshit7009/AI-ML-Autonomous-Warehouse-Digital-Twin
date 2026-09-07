c14f919a32f678b0dd5e1aed1071280ce4173e97
parikshit7009 <parikshit07009@gmail.com>
2026-09-08 00:28:05 +0530

    fastapi setup

---
+++ backend/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="AI/ML Warehouse Digital Twin",
+    description="Warehouse Digital Twin and What-If Simulator",
+    version="1.0.0"
+)
+
+@app.get("/")
+def root():
+    return {
+        "message": "Warehouse Digital Twin API is running"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
