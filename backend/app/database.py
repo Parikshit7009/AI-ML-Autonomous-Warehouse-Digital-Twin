846cd767801e17eaeaa6f4cbfaab578b6819fd79
parikshit7009 <parikshit07009@gmail.com>
2026-09-11 00:21:09 +0530

    func name change

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,5 +1,5 @@
 import mysql.connector as mysq
-def database():
+def db():
     con=mysq.connect(host="localhost",user="root",password="6618",database="warehouse_twin")
     cur=con.cursor()
     print("successfully connected")
