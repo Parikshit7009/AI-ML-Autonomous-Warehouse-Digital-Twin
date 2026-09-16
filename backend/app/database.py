02221bf67d629bfc9506aabc8d88fc381c142866
neerajsharma98914-svg <neeraj.sharma98914@gmail.com>
2026-09-16 16:07:52 +0530

    database setup on laptop2

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,8 +3,9 @@ import mysql.connector
 
 def get_database_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="6618",
+        host="10.211.136.187",
+        port=3306,
+        user="warehouse_user",
+        password="Warehouse@123",
         database="warehouse_twin"
     )
