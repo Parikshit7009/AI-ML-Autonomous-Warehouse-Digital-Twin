2df6464184956404a8c23d4203bd830036b4199a
neerajsharma98914-svg <neeraj.sharma98914@gmail.com>
2026-09-22 00:26:01 +0530

    sql connect

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,11 +2,9 @@ import mysql.connector
 
 
 def get_database_connection():
-
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="6618",
-        database="warehouse_twin",
-        port=3306
+        password="1638",
+        database="warehouse_twin"
     )
--- backend/import_warehouse_operations.py
+++ backend/import_warehouse_operations.py
@@ -19,7 +19,7 @@ CSV_FILE = BASE_DIR / "data" / "iot_warehouse_optimization_dataset.csv"
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="6618",
+    password="1638",
     database="warehouse_twin"
 )
 
